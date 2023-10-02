5edd0ca69fe4747d4f5f1b108d986d0ccbfd579e
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-10-02 19:38:18 +0300

    asc/desc save while loading

--- scripts/script.js
+++ scripts/script.js
@@ -35,16 +35,16 @@ function addTask(text, image, date) {
     var list = document.getElementById('task-list');
 
     var item = document.createElement('li');
-    
+
     var checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.addEventListener('change', completeTask);
 
     var taskText = document.createElement('span');
-    
+
     var todoItem;
-    
-    if (image) { 
+
+    if (image) {
         todoItem = new TodoItemPremium(text, image, date);
         taskText.innerText = todoItem.task;
 
@@ -52,13 +52,13 @@ function addTask(text, image, date) {
         img.src = todoItem.getTaskImage();
         img.style.width = '30px';
         img.style.height = '30px';
-        
+
         item.appendChild(img);
-    } else { 
+    } else {
         todoItem = new TodoItem(text, date);
         taskText.innerText = todoItem.task;
     }
-    
+
     taskText.addEventListener('dblclick', editTask);
 
     var taskDate = document.createElement('span');
@@ -71,7 +71,7 @@ function addTask(text, image, date) {
     remove.addEventListener('click', removeTask);
 
     buttons.appendChild(remove);
-    
+
     item.appendChild(checkbox);
     item.appendChild(taskText);
     item.appendChild(taskDate);
@@ -88,14 +88,14 @@ function addTask(text, image, date) {
 function removeTask(e) {
     var item = e.target.parentNode.parentNode;
     var parent = item.parentNode;
-    
+
     parent.removeChild(item);
     saveTasks();
 }
 
 function completeTask(e) {
     var item = e.target.parentNode;
-    
+
     if (item.style.textDecoration == 'line-through') {
         item.style.textDecoration = 'none';
         item.firstChild.checked = false;
@@ -107,91 +107,98 @@ function completeTask(e) {
 }
 
 function editTask(e) {
-  var item = e.target;
-  var originalText = item.innerText;
+    var item = e.target;
+    var originalText = item.innerText;
 
-  var inputField = document.createElement('input');
-  inputField.type = 'text';
-  inputField.value = originalText;
-  inputField.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-          saveTask(item, this, originalText);
-      } else if (event.key === 'Escape') {
-          cancelEdit(item, this, originalText);
-      }
-  });
+    var inputField = document.createElement('input');
+    inputField.type = 'text';
+    inputField.value = originalText;
+    inputField.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            saveTask(item, this, originalText);
+        } else if (event.key === 'Escape') {
+            cancelEdit(item, this, originalText);
+        }
+    });
 
-  item.innerText = '';
-  item.appendChild(inputField);
-  saveTasks();
+    item.innerText = '';
+    item.appendChild(inputField);
+    saveTasks();
 }
 
 function saveTask(item, inputField, originalText) {
     var newText = inputField.value;
-  
+
     if (newText && newText.trim() !== '') {
         item.innerText = newText;
-  
+
         var date = new Date();
         var dateString = ' (' + date.toLocaleDateString() + ' ' + date.toLocaleTimeString() + ')';
         item.nextSibling.innerText = dateString;
         var list = document.getElementById('task-list');
-        list.insertBefore(item.parentNode, list.childNodes[0]); 
+        list.insertBefore(item.parentNode, list.childNodes[0]);
     } else {
         cancelEdit(item, inputField, originalText);
     }
-  }
+}
 
 function cancelEdit(item, inputField, originalText) {
-  item.removeChild(inputField);
-  item.innerText = originalText;
+    item.removeChild(inputField);
+    item.innerText = originalText;
 }
 
 function removeCompletedTasks() {
-  var listItems = document.querySelectorAll('#task-list li');
+    var listItems = document.querySelectorAll('#task-list li');
 
-  for (var i=0; i<listItems.length; i++) {
-      if (listItems[i].firstChild.checked) {
-          listItems[i].parentNode.removeChild(listItems[i]);
-      }
-  }
+    for (var i = 0; i < listItems.length; i++) {
+        if (listItems[i].firstChild.checked) {
+            listItems[i].parentNode.removeChild(listItems[i]);
+        }
+    }
 }
 
 function removeAllTasks() {
     var listItems = document.querySelectorAll('#task-list li');
     var uncompletedExists = false;
-  
-    for (var i=0; i<listItems.length; i++) {
+
+    for (var i = 0; i < listItems.length; i++) {
         if (!listItems[i].firstChild.checked) {
             uncompletedExists = true;
             break;
         }
     }
-  
+
     if (uncompletedExists && !confirm("Are you sure you want to delete all tasks?")) {
         return;
     } else {
-        for (var i=0; i<listItems.length; i++) {
-        listItems[i].parentNode.removeChild(listItems[i]);
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].parentNode.removeChild(listItems[i]);
         }
     }
     saveTasks();
-  }
+}
 
 function sortTasksAsc() {
     var list = document.getElementById('task-list');
     Array.from(list.getElementsByTagName("li"))
-        .sort((a, b) => a.todoItem.date - b.todoItem.date)
+        .sort((a, b) => b.todoItem.date - a.todoItem.date)
         .forEach(li => list.appendChild(li));
+    document.getElementById('sort-asc').classList.add('active');
+    document.getElementById('sort-desc').classList.remove('active');
     saveTasks();
+    localStorage.setItem('sortOrder', 'asc');
 }
 
 function sortTasksDesc() {
     var list = document.getElementById('task-list');
     Array.from(list.getElementsByTagName("li"))
-        .sort((a, b) => b.todoItem.date - a.todoItem.date)
+        .sort((a, b) => a.todoItem.date - b.todoItem.date)
         .forEach(li => list.appendChild(li));
+
+    document.getElementById('sort-desc').classList.add('active');
+    document.getElementById('sort-asc').classList.remove('active');
     saveTasks();
+    localStorage.setItem('sortOrder', 'desc');
 }
 
 function saveTasks() {
@@ -203,6 +210,12 @@ function saveTasks() {
 function loadTasks() {
     var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     tasks.forEach(task => addTask(task.task, task.image, task.date));
+    var sortOrder = localStorage.getItem('sortOrder');
+    if (sortOrder === 'asc') {
+        sortTasksAsc();
+    } else if (sortOrder === 'desc') {
+        sortTasksDesc();
+    }
 }
 
 function clearStorage() {
